d2033f43d4a6628fa481c47092adb6691465104d
Anousha1846 <anoushaasadullah18@gmail.com>
2026-05-19 03:45:53 -0400

    Day 2 PSL Ex1 - variable declaration and reference type observation

---
+++ day2.java
@@ -0,0 +1,18 @@
+
+public class day2 {
+
+    public static void main(String[] args) {
+        int age = 25;
+        double temperature = 36.6;
+        boolean isLoggedIn = true;
+        char grade = 'A';
+        int[] scores = {90, 85, 78};
+
+        System.out.println(age);
+        System.out.println(temperature);
+        System.out.println(isLoggedIn);
+        System.out.println(grade);
+        System.out.println(scores);
+    }
+
+    }
