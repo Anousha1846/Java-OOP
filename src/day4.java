55309dd09166a91fc9e759effa13110a64cf3248
Anousha1846 <anoushaasadullah18@gmail.com>
2026-05-24 12:06:32 -0400

    Day 4 PSL Ex2 - factorial recursive

--- src/day4.java
+++ src/day4.java
@@ -1,12 +1,19 @@
 public class day4 {
-    public static int factorial(int n) {
-        int fact=1;
-        for (int i = n; i >= 1; i--) {
-            fact *= i;
+    // public static int factorial(int n) {
+    //     int fact=1;
+    //     for (int i = n; i >= 1; i--) {
+    //         fact *= i;
+    //     }
+    //     return fact;
+    // }
+    public static int factRecursive(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
         }
-        return fact;
+        return n*factRecursive(n-1);
     }
   public static void main(String[] args) {
-        System.out.println(factorial(8));
+        // System.out.println(factorial(8));//iterative
+        System.out.println(factRecursive(6));
   }
 }
