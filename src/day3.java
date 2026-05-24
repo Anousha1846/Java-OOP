c5cf9de56f608583fc77ee7eb4a053c73b6ee252
Anousha1846 <anoushaasadullah18@gmail.com>
2026-05-24 05:53:49 -0400

    Day 3 PSL Ex2 - right angle triangle of stars

--- src/day3.java
+++ src/day3.java
@@ -2,19 +2,26 @@
 // Most specific condition goes first, most general goes last.
 public class day3 {
     public static void main(String[] args) {
-        for(int i = 1; i<=100; i++){
-            if (i%3 == 0 && i%5 == 0) {
-                System.out.println  ("FizzBuzz");
-            }else if (i%3 == 0) {
-                System.out.println("Fizz");
-            }else if(i%5 == 0){
-                System.out.println("Buzz");
-            }else{
-                System.out.println(i);
-            }
-        }
+    //     for(int i = 1; i<=100; i++){
+    //         if (i%3 == 0 && i%5 == 0) {
+    //             System.out.println  ("FizzBuzz");
+    //         }else if (i%3 == 0) {
+    //             System.out.println("Fizz");
+    //         }else if(i%5 == 0){
+    //             System.out.println("Buzz");
+    //         }else{
+    //             System.out.println(i);
+    //         }
+    //     }
+    
+
+//PSL 2 — Right-angle triangle of stars
+for(int i = 1; i <= 5; i++){ 
+    for(int j = 1 ; j<=i; j++){
+    System.out.print("*");
     }
+    System.out.println();
 }
-
-
+}
+}
 
