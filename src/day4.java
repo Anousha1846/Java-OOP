7d1a09626326f30562637e0e1486f9f431912058
Anousha1846 <anoushaasadullah18@gmail.com>
2026-05-29 05:46:18 -0400

    Day 4 PSL Ex3 - string reverse recursive

--- src/day4.java
+++ src/day4.java
@@ -6,14 +6,28 @@ public class day4 {
     //     }
     //     return fact;
     // }
-    public static int factRecursive(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
+    // public static int factRecursive(int n) {
+    //     if (n == 0 || n == 1) {
+    //         return 1;
+    //     }
+    //     return n*factRecursive(n-1);
+    // }
+    public static void StringReverse(String str) {
+        
+        for(int i = str.length()-1 ; i>=0; i--){
+          System.out.print(str.charAt(i));
         }
-        return n*factRecursive(n-1);
+    }
+    public static String StrRevRecusive(String str) {
+      if (str.length() <=1) {
+        return str;
+      }
+      return str.charAt(str.length()-1) + StrRevRecusive(str.substring(0, str.length()-1));
     }
   public static void main(String[] args) {
         // System.out.println(factorial(8));//iterative
-        System.out.println(factRecursive(6));
+        // System.out.println(factRecursive(6));
+        StringReverse("hello");
+        System.out.println(StrRevRecusive("jonita"));
   }
 }
