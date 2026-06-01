1998cb84c9d8e7297aa0f7ffcb7b2deb550a9dbe
Anousha1846 <anoushaasadullah18@gmail.com>
2026-06-01 08:33:55 -0400

    Day 5 Step 3 - array search operation

--- src/day5.java
+++ src/day5.java
@@ -1,16 +1,30 @@
 public class day5 {
-    public static void main(String[] args) {
-        int[][] arr = {
-            {1,2,3},
-            {4,5,6},
-            {7,8,9}
-        };
-        //print
-        for(int i = 0; i<arr.length; i++){
-            for (int j = 0; j < arr[i].length ; j++) {
-                System.out.print(arr[i][j] + " ");
+    public static int search(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; 
             }
-            System.out.println();
         }
+            return -1;
+    }
+    public static void main(String[] args) {
+        // int[][] arr = {
+        //     {1,2,3},
+        //     {4,5,6},
+        //     {7,8,9}
+        // };
+        // //print
+        // for(int i = 0; i<arr.length; i++){
+        //     for (int j = 0; j < arr[i].length ; j++) {
+        //         System.out.print(arr[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        int[] array = {3, 7, 1, 6, 9};
+        search(array, 2);
+        System.out.println("found at " + search(array, 6));
+System.out.println("found at " + search(array, 2));
+        
     }
 }
