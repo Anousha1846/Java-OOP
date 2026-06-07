2f280aaaf345ea45c93f779de3b3152533a30f37
Anousha1846 <anoushaasadullah18@gmail.com>
2026-06-07 14:30:20 -0400

    Day 8 - Maven project setup with JUnit 5

---
+++ oop-testing/src/main/java/com/javaoop/App.java
@@ -0,0 +1,13 @@
+package com.javaoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
