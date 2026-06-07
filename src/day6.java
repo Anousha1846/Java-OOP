d64b70545366af13f95b2bf7fd96a56bc5aadd25
Anousha1846 <anoushaasadullah18@gmail.com>
2026-06-07 10:16:47 -0400

    Day 6 PSL - Student class with 3 objects

--- src/day6.java
+++ src/day6.java
@@ -1,3 +1,4 @@
+//PSL-1
 class BankAccount {
     String owner;
     double balance;
@@ -15,12 +16,34 @@ class BankAccount {
     }
 
 }
+//PSL-2
+class Student{
+    String name;
+    int age;
+    double grade;
+    public Student(String name, int age, double grade){
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+    public void display(){
+        System.out.println("name: "+  name + "\n age: " + age+ "\ngrade: " + grade);
+    }
+}
+
 public class day6 {
     public static void main(String[] args) {
+        //PSL-1
     BankAccount account1 = new BankAccount("Alice", 1000.0);
     BankAccount account2 = new BankAccount();
 
     account1.display();
     account2.display();
+
+    //PSL-2
+    Student s1 = new Student("Ali", 22, 1);
+    Student s2 = new Student("Alina", 22, 1);
+    Student s3 = new Student("sara", 22, 1);
+
 }
 }
